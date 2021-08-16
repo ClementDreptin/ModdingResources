4a91df89c9fc4cfe1153b2be1bf2d38a58d70cc6
Clement <cdreptin@gmail.com>
2021-08-17 01:39:54 +0200

    Made a solution for the example project in the getting started section

---
+++ GettingStarted/GettingStarted/GettingStarted/src/main.cpp
@@ -0,0 +1,26 @@
+#include "pch.h"
+
+// Gets the address of the function within a module by its ordinal
+DWORD ResolveFunction(LPCSTR moduleName, DWORD ordinal)
+{
+    HMODULE mHandle = GetModuleHandle(moduleName);
+
+	return (mHandle == NULL) ? NULL : (DWORD)GetProcAddress(mHandle, (LPCSTR)ordinal);
+}
+
+// Creates a function pointer from the address of XNotifyQueueUI retrieved by ResolveFunction
+typedef VOID (*XNOTIFYQUEUEUI)(DWORD exnq, DWORD dwUserIndex, ULONGLONG qwAreas, PWCHAR displayText, PVOID contextData);
+XNOTIFYQUEUEUI XNotifyQueueUI = (XNOTIFYQUEUEUI)ResolveFunction("xam.xex", 656);
+
+BOOL APIENTRY DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpReserved)
+{
+    switch (fdwReason) 
+    {
+        case DLL_PROCESS_ATTACH:
+			XNotifyQueueUI(0, 0, XNOTIFY_SYSTEM, L"Hello World!", 0);
+            break;
+        case DLL_PROCESS_DETACH:
+            break;
+    }
+    return TRUE;
+}
---
+++ GettingStarted/GettingStarted/GettingStarted/src/pch.h
@@ -0,0 +1,4 @@
+#pragma once
+
+#include <xtl.h>
+#include <xboxmath.h>
